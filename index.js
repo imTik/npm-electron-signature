74a26f977c584d73cc92c9c046674f0d2262ef4a
Tik Guan <275151754@qq.com>
2020-06-16 14:50:21 +0800

    init electron signature

---
+++ index.js
@@ -0,0 +1,165 @@
+class Signature {
+
+  /**
+   * 
+   * @param { String } id          容器ID
+   * @param { Number } height      画布高度
+   * @param { String } background  画布背景
+   * @param { Boolean } resetBtn   重签按钮
+   * @param { Number } lineWidth   线条粗细
+   * @param { String } lineColor   线条颜色
+   */
+  constructor ({ 
+    id, 
+    height = 200,
+    background = 'rgba(248, 248, 248, 1)',
+    resetBtn = true,
+    lineWidth = 1,
+    lineColor = '#000000'
+  }) {
+
+    // options
+    this.background = background;
+    this.height = height;
+    this.container = document.getElementById(id);
+    this.container.style = 'position: relative';
+    this.mouseDown = false;
+
+    this.strokeRecord = [];
+
+    // 创建/设置 画布
+    this.canvas = document.createElement('canvas');
+    this.adapt();
+
+    if (resetBtn) this.addResetBtn();
+
+    this.container.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+    this.setLineStyle(lineWidth, lineColor);
+    this.initBg();
+
+    this.mobileTouchEvent();
+    this.pcMouseEvent();
+
+    window.addEventListener('resize', e => {
+      this.adapt();
+      this.initBg();
+      this.reductionStroke()
+    });
+  }
+
+  setLineStyle (width, color) {
+    this.ctx.lineWidth = width;
+    this.ctx.strokeStyle = color;
+  }
+
+  initBg () {
+    this.ctx.fillStyle = this.background;
+    this.ctx.fillRect(0, 0, this.width, this.height);
+  }
+
+  // 自适应宽度
+  adapt () {
+    this.width = this.container.clientWidth;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+  }
+
+  mobileTouchEvent () {
+
+    this.canvas.addEventListener('touchstart', e => {
+      this.ctx.beginPath();
+      let { x, y } = this.getCoordinate(e);
+      this.ctx.moveTo(x, y);
+      this.recordStroke({x, y});
+    });
+
+    this.canvas.addEventListener('touchmove', e => {
+      e.preventDefault();
+      let { x, y } = this.getCoordinate(e);
+      this.ctx.lineTo(x, y);
+      this.ctx.stroke();
+      this.recordStroke({x, y});
+    });
+
+    this.canvas.addEventListener('touchend', e => {
+      this.ctx.closePath();
+    });
+  }
+
+  pcMouseEvent () {
+    this.canvas.addEventListener('mousedown', e => {
+      this.mouseDown = true;
+      this.ctx.beginPath();
+      let { x, y } = this.getCoordinate(e);
+      this.ctx.moveTo(x, y);
+      this.recordStroke({x, y});
+    });
+
+    this.canvas.addEventListener('mousemove', e => {
+      if (!this.mouseDown) return;
+      let { x, y } = this.getCoordinate(e);
+      this.ctx.lineTo(x, y);
+      this.ctx.stroke();
+      this.recordStroke({x, y});
+    });
+
+    this.canvas.addEventListener('mouseup', e => {
+      this.mouseDown = false;
+      this.ctx.closePath();
+    });
+
+    this.canvas.addEventListener('mouseover', e => {
+      this.mouseDown = false;
+      this.ctx.closePath();
+    })
+  }
+
+  // 记录笔画
+  recordStroke (point) {
+    this.strokeRecord.push(point);
+  }
+
+  // 还原笔画
+  reductionStroke () {
+    this.ctx.beginPath();
+    this.strokeRecord.forEach(item => this.ctx.lineTo(item.x, item.y));
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+
+  // 获取坐标
+  getCoordinate (e) {
+    let touch = e.targetTouches ? e.targetTouches[0] : e;
+    let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    let x = touch.pageX - this.container.offsetLeft;
+    let y = touch.pageY - this.container.offsetTop + scrollTop;
+    return { x, y };
+  }
+
+  addResetBtn () {
+    let span = document.createElement('span');
+    span.innerHTML = '重签';
+    span.style = 'position: absolute; color: #0071CE; right: 15px; bottom: 15px; font-size: 14px; cursor: pointer;'
+    this.container.appendChild(span);
+    span.addEventListener('click', e => {
+      this.cleanStroke();
+    });
+  }
+
+  cleanStroke () {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.initBg();
+    this.strokeRecord = [];
+  }
+
+  // 画布截屏图片
+  getImage () {
+    var MIME_TYPE = "image/png";
+    var imgURL = this.canvas.toDataURL(MIME_TYPE);
+    return imgURL;
+  }
+
+}
+
+export default Signature;
---
+++ readme.md
@@ -0,0 +1,38 @@
+# electron-signature 
+
+``` 
+npm install electron-signature
+```
+
+## 使用
+``` javascript
+import Signature from 'electron-signature';
+
+<div id="signature-container"></div>
+
+let signature = new Signature({
+  id: 'signature-container',
+  lineWidth: 2,
+});
+
+```
+
+## 初始化参数
+
+参数 | 类型 | 说明 | 是否必填
+- | - | - | - |
+id  | String | 容器ID | Y
+height | Number | 画布高度 | N
+background | String | 画布背景 | N
+resetBtn | Boolean | 是否显示重签按钮 | N
+lineWidth | Number | 线条粗细 | N
+lineColor | String | 线条颜色 | N
+
+
+## 实例 事件/参数
+
+事件/参数 | 类型 | 说明 
+ - | - | -
+getImage | function | 获取签名图片
+cleanStroke | function | 清空笔画
+strokeRecord | field | 笔画点记录
